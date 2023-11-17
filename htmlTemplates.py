14e1c53b7dcbef1a396a86cc890df8f10789f787
TakutoIyanagi-littletree <131866009+TakutoIyanagi-littletree@users.noreply.github.com>
2023-11-18 04:01:26 +0900

    Update htmlTemplates.py

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -28,7 +28,7 @@ css = '''
 bot_template = '''
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="https://ibb.co/5BwMb6X/bot.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        <img src="https://i.ibb.co/FgCxRKk/bot.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
     </div>
     <div class="message">{{MSG}}</div>
 </div>

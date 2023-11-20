2bdbfd14b21d7325c8710faf9e45d5d890f8cc97
TakutoIyanagi-littletree <131866009+TakutoIyanagi-littletree@users.noreply.github.com>
2023-11-21 01:20:16 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,58 +1,90 @@
-import os
-import openai
 import streamlit as st
+import openai
 from dotenv import load_dotenv
-from langchain.agents import AgentType, initialize_agent, load_tools
-from langchain.callbacks import StreamlitCallbackHandler
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
-from langchain.prompts import MessagesPlaceholder
-from langchain.vectorstores import FAISS
-from langchain.tools.base import BaseTool
-from langchain.chains import RetrievalQA
-from langchain.prompts import PromptTemplate
-from langchain.agents import AgentType, initialize_agent, load_tools
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+from langchain.llms import HuggingFaceHub
+from langchain.document_loaders import PyPDFLoader
+
+pdf_docs = PyPDFLoader("yoran_jyouhou.pdf")
+
+#PDFを受け取る
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+#PDFのテキストをチャンク毎に分ける
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+#FAISSを使って、ベクトルを格納
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+#会話履歴等を保存し、会話のチャットを作り出す
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k")
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True
+    )
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+#これまで定義したものをStreamlitで表示する
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
 
 
-load_dotenv()
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="TokAI 1.0",
+                       page_icon="🤖")
+    st.write(css, unsafe_allow_html=True)
 
-#openai_api_key=os.environ["OPENAI_API_KEY"]
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
 
-# エージェントチェーンの作成
-def create_agent_chain():
-    ### FAISS vectorのロード
-    vectoreStore = FAISS.load_local("faiss_index/", OpenAIEmbeddings())
-    ## Retriever
-    retriever = vectoreStore.as_retriever(search_type="similarity", search_kwargs={"k":3})
-    ### プロンプト(Q&A)
-    qa = RetrievalQA.from_chain_type(llm=ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k"), chain_type="stuff", retriever=retriever, return_source_documents=False)
+    st.header("TokAI 1.0 🤖")
+    user_question = st.text_input("東海大学に関する質問は、なんでもどうぞ")
+    if user_question:
+        handle_userinput(user_question)
 
-
-if "agent_chain" not in st.session_state:
-    st.session_state.agent_chain = create_agent_chain()
+        
 
 
-st.title("langchain-streamlit-app")
-
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-prompt = st.chat_input("What is up?")
-
-if prompt:
-    st.session_state.messages.append({"role": "user", "content": prompt})
-
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    with st.chat_message("assistant"):
-        callback = StreamlitCallbackHandler(st.container())
-        response = st.session_state.agent_chain.run(prompt, callbacks=[callback])
-        st.markdown(response)
-
-    st.session_state.messages.append({"role": "assistant", "content": response})
+if __name__ == '__main__':
+    main()
